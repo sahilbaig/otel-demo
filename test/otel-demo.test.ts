62a1d89cb6031d55899a84e51517b0762d6a3f29
sahilbaig <sahilbaig.me@gmail.com>
2026-05-09 13:54:05 +0530

    Initial commit

---
+++ test/otel-demo.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as OtelDemo from '../lib/otel-demo-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/otel-demo-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new OtelDemo.OtelDemoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
